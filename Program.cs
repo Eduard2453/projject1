010c661bc3880bceb1068e81bf1fd78b4e65c4c1
Eduard2453 <117133762+Eduard2453@users.noreply.github.com>
2026-09-12 15:45:19 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+        static void Main()
+    {
+        Console.Write("ВВЕДИТЕ ДЛИНУ");
+        double a = double.Parse(Console.ReadLine().Replace(',', '.'));
+
+        Console.Write("ВВЕДИТЕ  Высоту");
+        double b = double.Parse(Console.ReadLine().Replace(',', '.'));
+        double area = a * b;
+        double perimeter = 2 * (a + b);
+        Console.WriteLine($"ploshad {area}");
+        Console.WriteLine($"perimetr{perimeter}");
+    }
+}
